c9d65713e060b79448f6f882de2fe0bbd5b38a17
Sarl James Sebios <ssarljames@hotmail.com>
2020-04-12 01:26:07 +0800

    make it better

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -27,35 +27,37 @@ class UsersTableSeeder extends Seeder
             'email' => 'ssarljames@hotmail.com'
         ]);
 
-        for($i=0; $i < 5; $i++)
-            DB::table('users')->insert([
-                'username' => 'user' . ($i + 1),
-                'password' => bcrypt('password'),
-                'firstname' => $factory->firstName,
-                'lastname' => $factory->lastName,
-                'email' => $factory->email
-            ]);
-
-
-        $this->command->info('Seeding projects...');
-
-        for($i=0; $i < 2; $i++){
-           Project::create([
-                'title' => 'Project ' . ($i + 1),
-                'description' => $factory->realText,
-                'user_id' => 1
-            ]);
-
-        }
-
-        $this->command->info('Seeding project members...');
-
-
-        for($i=1; $i <= 2; $i++)
-            for($j=2; $j < 5; $j++)
-                DB::table('project_members')->insert([
-                    'project_id' => $i,
-                    'user_id' => $j
+        if(env('APP_ENV', 'local') == 'local'){
+            for($i=0; $i < 5; $i++)
+                DB::table('users')->insert([
+                    'username' => 'user' . ($i + 1),
+                    'password' => bcrypt('password'),
+                    'firstname' => $factory->firstName,
+                    'lastname' => $factory->lastName,
+                    'email' => $factory->email
                 ]);
+
+
+            $this->command->info('Seeding projects...');
+
+            for($i=0; $i < 2; $i++){
+            Project::create([
+                    'title' => 'Project ' . ($i + 1),
+                    'description' => $factory->realText,
+                    'user_id' => 1
+                ]);
+
+            }
+
+            $this->command->info('Seeding project members...');
+
+
+            for($i=1; $i <= 2; $i++)
+                for($j=2; $j < 5; $j++)
+                    DB::table('project_members')->insert([
+                        'project_id' => $i,
+                        'user_id' => $j
+                    ]);
+        }
     }
 }
